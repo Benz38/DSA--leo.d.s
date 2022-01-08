5ef1a07ec43efcdcba3b229af4deca5533781c4f
Deepak <leodskushwah@gmail.com>
2022-01-08 12:48:31 +0530

    push

--- jaiho.cpp
+++ jaiho.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-cout<<"---jai ho---";
-return 0;
-}
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int maxi = INT_MIN; 
+        for(auto it : nums) {
+            sum += it;
+            maxi = max(sum, maxi); 
+            if(sum < 0) sum = 0;
+        }
+        return maxi; 
+    }
+};
